be2c1a4370e338bf77e6940e0846055884d07902
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2023-04-28 20:05:30 +0530

    Inin

---
+++ src/main/java/com/tailor/Shop/TailorShopApplication.java
@@ -0,0 +1,14 @@
+package com.tailor.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailorShopApplication.class, args);
+		System.out.println("Application started...............................");
+	}
+
+}
---
+++ src/test/java/com/tailor/Tailor/Shop/TailorShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailor.Tailor.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
